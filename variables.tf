fd4e459f5072e020566f26cbeb9b3ae778b90c09
mhagi00 <48246117+mhagi00@users.noreply.github.com>
2021-07-21 21:07:15 +0100

    update var

--- variables.tf
+++ variables.tf
@@ -1,34 +1,35 @@
 
 variable "public_key" {
   description = "The public API key for MongoDB Atlas"
-  default = "12345"
+  default     = "12345"
+  type        = string
 }
 variable "private_key" {
   description = "The private API key for MongoDB Atlas"
   default     = "12345"
-  type = string
+  type        = string
 }
 
 variable "project_name" {
   description = "The name of the project you want to create"
   type        = string
-  default     = "projectname"
+  default     = "atlas"
 }
 
 variable "org_id" {
   description = "The ID of the Atlas organization you want to create the project within"
   type        = string
-  default = "org"
+  default     = "org123"
 }
 
 variable "mongdbatlas_project_id" {
   description = "Project ID"
-  default = "projectid"
+  default     = "projectid"
 }
 
 variable "environment" {
   description = "username"
   type        = list(string)
-  default     = ["possums-data-store", "numbats-data-store", "marsupial-data-store"]
+  default     = ["possums-data-store","numbats-data-store","marsupial-data-store"]
 }
  
